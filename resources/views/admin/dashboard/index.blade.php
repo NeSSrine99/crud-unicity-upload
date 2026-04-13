64283a1d00864f6d02f5e510cfbc94acbc12ddef
Nesrine Nasri <nesrinasri99@gmail.com>
2026-04-13 11:15:16 +0100

    create dashboard structure

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.layout.layout')
+
+@section('title', 'index | Dashboard')
+
+@section('content')
+    <div class="container">
+        <h1>Dashboard</h1>
+        <p> Welcome to the dashboard!</p>
+
+    </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\TeacherController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('teachers', TeacherController::class);
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
